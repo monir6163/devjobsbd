8b6aa248e537d09fdd1b5746a52c8a2a78fb33fb
netstermonir <monirhossain6163@gmail.com>
2024-03-08 17:49:39 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,9 @@ export default function Navbar() {
             <ModeToggle />
           </nav>
           <div className="block lg:hidden">
+            <ModeToggle />
+          </div>
+          <div className="block lg:hidden">
             <button
               onClick={toggleMenu}
               className="flex items-center px-3 py-2 border rounded text-gray-700 dark:text-gray-100 border-gray-400 dark:border-gray-600 hover:text-gray-900 hover:border-gray-900"
